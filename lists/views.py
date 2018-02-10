c8ff6e501cad420f56a89ea1d555561d0a75e7fb
Shiftypanda <drtimddavis@gmail.com>
2018-02-10 11:31:29 +0000

    Added unit test for checking post return

--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
     return render(request, 'lists/home.html')
