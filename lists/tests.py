1bca856acdc8b1157391ddbdb73c26826ea6a468
Shiftypanda <drtimddavis@gmail.com>
2018-02-10 11:27:27 +0000

    updated test and html to show basic views

--- lists/tests.py
+++ lists/tests.py
@@ -2,10 +2,11 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
