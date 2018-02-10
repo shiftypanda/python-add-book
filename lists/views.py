8fd456e77e68da067eda05b0646404cfce7db7ef
Shiftypanda <drtimddavis@gmail.com>
2018-02-10 11:38:07 +0000

    updated so unit tests pass for rendering responset to POST or empty POST

--- lists/views.py
+++ lists/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    if request.method == 'POST':
-        return HttpResponse(request.POST['item_text'])
-    return render(request, 'lists/home.html')
+    return render(request, 'lists/home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
