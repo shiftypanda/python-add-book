06199276a818e3472aa3a2ed8aa5b22c0a7d85bd
Shiftypanda <drtimddavis@gmail.com>
2018-02-16 08:44:21 +0000

    new URL + view for adding to exitinsg lists. FT passes :)

--- lists/tests.py
+++ lists/tests.py
@@ -57,6 +57,12 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'lists/list.html')
 
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
+
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
         Item.objects.create(text='itemey 1', list=correct_list)
@@ -73,6 +79,8 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other list item 2')
 
 
+
+
 class NewListTest(TestCase):
 
         def test_can_save_a_POST_request(self):
@@ -86,3 +94,28 @@ class NewListTest(TestCase):
             response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
             new_list = List.objects.first()
             self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(f'/lists/{correct_list.id}/add_item',
+        data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(f'/lists/{correct_list.id}/add_item',
+        data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
--- lists/views.py
+++ lists/views.py
@@ -8,8 +8,7 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
-    return render(request, 'lists/list.html', {'items': items})
+    return render(request, 'lists/list.html', {'list': list_})
 
 def new_list(request):
     list_ = List.objects.create()
@@ -17,3 +16,8 @@ def new_list(request):
     return redirect(f'/lists/{list_.id}/')
 
 # TODO: 7.11 Function test detect another regression (16/2/2018)
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
