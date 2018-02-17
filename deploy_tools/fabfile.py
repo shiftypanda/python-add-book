bfb9df981792d94f1296eb67988f61670d68e857
Shiftypanda <drtimddavis@gmail.com>
2018-02-17 10:33:02 +0000

    corrected typo in fab file

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -5,7 +5,7 @@ from fabric.api import cd, env, local, run
 REPO_URL = 'https://github.com/shiftypanda/python-tdd-book.git'
 
 def deploy():
-    site_folder = f'/home/{env.user}/sites/{env.host}
+    site_folder = f'/home/{env.user}/sites/{env.host}'
     run(f'mkdir -p {site_folder}')
     with cd(site_folder):
         _get_latest_source()
