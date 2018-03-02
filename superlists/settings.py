a6585f891295bdcbd02651c4643ca4f1151fc86d
Shiftypanda <drtimddavis@gmail.com>
2018-03-02 14:45:59 +0000

    Started accounts work

--- superlists/settings.py
+++ superlists/settings.py
@@ -32,6 +32,22 @@ else:
     DEBUG = True
     SECRET_KEY = 'insecure-key-for-dev'
     ALLOWED_HOSTS = []
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+            },
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['console'],
+            },
+        },
+        'root': {'level': 'INFO'},
+    }
 
 
 # Application definition
