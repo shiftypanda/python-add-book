4d98bcc1a3df373a0d195f177dea6d69672ae2c3
Shiftypanda <drtimddavis@gmail.com>
2018-03-02 23:49:06 +0000

    Added error message to base and mock using unittest patch

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
+from django.contrib import messages
 # Create your views here.
 
 def send_login_email(request):
     email = request.POST['email']
+    print(type(send_mail))
     send_mail(
     'Your login link for Superlists',
-    'body text to be confirmed', 
+    'Use this link to log in',
     'noreply@superlists',
     [email]
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
     )
     return redirect('/')
