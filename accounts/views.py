3b0d854bdbf3cd06cb384003889218147e47f0c6
Shiftypanda <drtimddavis@gmail.com>
2018-03-03 00:27:56 +0000

    Authentication backend passwordless created and tests passing, views to be done

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
+from django.core.urlresolvers import reverse
+from accounts.models import Token
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.contrib import messages
@@ -5,10 +7,14 @@ from django.contrib import messages
 
 def send_login_email(request):
     email = request.POST['email']
-    print(type(send_mail))
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
     send_mail(
     'Your login link for Superlists',
-    'Use this link to log in',
+    message_body,
     'noreply@superlists',
     [email]
     )
@@ -16,4 +22,7 @@ def send_login_email(request):
         request,
         "Check your email, we've sent you a link you can use to log in."
     )
+    return redirect('/')
+
+def login(request):
     return redirect('/')
